ad9dc0094bce8af4be8195057ebf0a65f1d2117f
takhyunKim <kimkih1218@gmail.com>
2021-04-15 22:01:48 +0900

    Add: redux & router init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
+import reducer from "./reducers";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
+
+import App from "./containers/App";
+
+const middleware = [];
+
+if (process.env.NODE_ENV !== "production") {
+  middleware.push(createLogger());
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
