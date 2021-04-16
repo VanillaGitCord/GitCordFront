e06f815020263082aef5e7bb9fae33050dc5a0c9
Jay_WooKJ <natcho9010@gmail.com>
2021-04-16 17:52:28 +0900

    Add: WelcomeHeader component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
 import Background from "../publicComponents/Backgroud/Background";
+import WelcomeHeader from "../publicComponents/WelcomeHeader/WelcomeHeader";
+import LoginContainer from "./LoginContainer/LoginContainer";
+
+const LoginOutter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
 
 function Login() {
   return (
     <Background>
-      <section>
-        adf
-      </section>
+      <LoginOutter>
+        <WelcomeHeader>
+        </WelcomeHeader>
+        <LoginContainer>
+        </LoginContainer>
+      </LoginOutter>
     </Background>
   );
 }
---
+++ src/components/Login/LoginContainer/LoginContainer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoginContinerStyle = styled.div`
+  width: 40vw;
+  height: 60vh;
+  border: 3px solid black;
+`;
+
+function LoginContainer() {
+  return (
+    <LoginContinerStyle>
+
+    </LoginContinerStyle>
+  );
+}
+
+export default LoginContainer;
