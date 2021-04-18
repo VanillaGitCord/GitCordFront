a6837e4e4c282923cf05575a33f92b16b1dab636
Jay_WooKJ <natcho9010@gmail.com>
2021-04-18 11:02:56 +0900

    Add: login logic in LoginContainer

--- src/components/Login/LoginContainer/LoginContainer.jsx
+++ src/components/Login/LoginContainer/LoginContainer.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router";
 import styled from "styled-components";
 
 import InputWithLabel from "../../publicComponents/InputWithLabel/InputWithLabel";
@@ -29,7 +30,23 @@ const LoginContainerStyle = styled.div`
     justify-content: space-evenly;
     align-items: center;
     width: 60%;
-    height: 70%;
+    height: 80%;
+
+    &-email {
+      width: 100%;
+      height: 20%;
+    }
+
+    &-password {
+      width: 100%;
+      height: 20%;
+    }
+  }
+
+  .error {
+    color: red;
+    font-size: 15px;
+    font-weight: bold;
   }
 
   .login-signup {
@@ -43,25 +60,85 @@ const LoginContainerStyle = styled.div`
   }
 `;
 
+function isEmailValidate(email) {
+  return email.match(/\w+@\w+.\w+/g);
+}
+
 function LoginContainer() {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  async function handleLoginClick() {
+    if (!email) return setEmailError("E-mail을 입력해주세요!");
+
+    if (!isEmailValidate(email)) return setEmailError("E-mail 형식을 맞춰주세요! ex) 123@asd.com");
+
+    if (!password) return setPasswordError("Password를 입력해주세요!");
+
+    const loginInfo = {
+      email,
+      password
+    };
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/user`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        mode: "cors",
+        body: JSON.stringify(loginInfo)
+      });
+
+      if (response.message) throw new Error(response.message);
+    } catch (err) {
+      setIsError(true);
+    }
+  }
+
   return (
     <LoginContainerStyle>
       <div className="login-title">Login</div>
-      <div className="login-contents">
-        <InputWithLabel
-          labelContent="Email"
-          placeholder="Type email here"
-          height="20%"
-        />
-        <InputWithLabel
-          labelContent="Password"
-          placeholder="Type password"
-          height="20%"
-        />
+      <section className="login-contents">
+        <div className="login-contents-email">
+          <InputWithLabel
+            labelContent="Email"
+            placeholder="Type email here"
+            height="95%"
+            onChange={handleEmailChange}
+          />
+          <div className="error">
+            {emailError}
+          </div>
+        </div>
+        <div className="login-contents-password">
+          <InputWithLabel
+            labelContent="Password"
+            placeholder="Type password"
+            height="95%"
+            onChange={handlePasswordChange}
+            type="password"
+          />
+          <div className="error">
+            {passwordError}
+          </div>
+        </div>
         <Button
           content="LOGIN"
           width="40%"
           height="10%"
+          onClick={handleLoginClick}
         />
         <Button
           content="GOOGLE LOGIN"
@@ -70,8 +147,9 @@ function LoginContainer() {
           backgroundColor="#0C59CF"
           color="white"
         />
-        <a className="login-signup">Sign up</a>
-      </div>
+        <a className="login-signup" href="/signup">Sign up</a>
+      </section>
+      { isError && <Redirect to="/error" /> }
     </LoginContainerStyle>
   );
 }
