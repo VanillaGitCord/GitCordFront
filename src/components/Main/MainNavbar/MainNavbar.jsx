2254fde8a26128c9d2579ea1dcee4d2c4d0b1975
Jay_WooKJ <natcho9010@gmail.com>
2021-04-18 22:21:41 +0900

    Add: logout logic

--- src/components/Main/MainNavbar/MainNavbar.jsx
+++ src/components/Main/MainNavbar/MainNavbar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Redirect } from "react-router";
+import { useDispatch } from "react-redux";
 import { ImArrowLeft } from "react-icons/im";
 import { GiExitDoor } from "react-icons/gi";
 import { FaUserCircle } from "react-icons/fa";
 import styled from "styled-components";
+
+import { deleteUser } from "../../../actions/userActions";
+import { deleteRoom } from "../../../actions/roomActions";
 
 import MainIcon from "../../publicComponents/MainIcon/MainIcon";
 
@@ -25,10 +30,24 @@ const MainNavbarContainer = styled.div`
     width: 15vw;
     height: 5vh;
     line-height: 5vh;
+
+    &-logout {
+      cursor: pointer;
+    }
   }
 `;
 
 function MainNavbar() {
+  const dispatch = useDispatch();
+
+  function handleLogoutButtonClick() {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    dispatch(deleteUser());
+    dispatch(deleteRoom());
+    return <Redirect to="/login" />;
+  }
+
   return (
     <MainNavbarContainer>
       <div className="navbar-left">
@@ -37,7 +56,11 @@ function MainNavbar() {
         Room name
       </div>
       <div className="navbar-right">
-        <GiExitDoor size={30} />
+        <GiExitDoor
+          className="navbar-right-logout"
+          size={30}
+          onClick={handleLogoutButtonClick}
+        />
         <FaUserCircle size={30} />
         User email
       </div>
