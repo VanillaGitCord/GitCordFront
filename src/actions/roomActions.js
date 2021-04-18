afeedef20c0e4482ed2acd2ae18928b60ee68eb9
takhyunKim <kimkih1218@gmail.com>
2021-04-18 17:10:35 +0900

    Add: room Action, reducer, actionTypes

--- src/actions/roomActions.js
+++ src/actions/roomActions.js
@@ -1,7 +1,24 @@
-import { CREATE_ROOM } from "../constants/actionTypes";
+import {
+  CREATE_ROOM,
+  CREATE_ROOM_SUCCESS,
+  CREATE_ROOM_FAILURE
+} from "../constants/actionTypes";
 
 export function createRoom() {
   return {
     type: CREATE_ROOM
+  };
+}
+
+export function createRoomSuccess(roomId) {
+  return {
+    type: CREATE_ROOM_SUCCESS,
+    payload: roomId
+  };
+}
+
+export function createRoomFailure() {
+  return {
+    type: CREATE_ROOM_FAILURE
   };
 }
--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,3 +1,5 @@
 export const CREATE_ROOM = "CREATE_ROOM";
+export const CREATE_ROOM_SUCCESS = "CREATE_ROOM_SUCCESS";
+export const CREATE_ROOM_FAILURE = "CREATE_ROOM_FAILURE";
 
 export const ADD_USER = "ADD_USER";
