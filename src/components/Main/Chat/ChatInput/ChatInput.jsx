654e226326621401452a2ab602d576383a1fda20
takhyunKim <kimkih1218@gmail.com>
2021-04-18 10:53:38 +0900

    Update: Chat, CahtInput Component style

--- src/components/Main/Chat/ChatInput/ChatInput.jsx
+++ src/components/Main/Chat/ChatInput/ChatInput.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { AiOutlineEnter } from "react-icons/ai";
 import styled from "styled-components";
 
 const ChatInputContainer = styled.div`
   width: 100%;
   height: 10%;
-  padding-top: 1em;
   border-top: 1px solid #FFEEF4;
+
+  .chat-input {
+    width: 50%;
+    margin: 0.8em;
+    border: 2px solid #c238eb;
+    border-radius: 5px;
+    outline: none;
+  }
+
+  .chat-button {
+    background: none;
+    border: 2px solid #c238eb;
+    border-radius: 10px;
+    cursor: pointer;
+  }
 `;
 
 function ChatInput({
@@ -21,8 +36,11 @@ function ChatInput({
           placeholder="chat.."
           value={chat}
           onChange={handleChatChange}
+          className="chat-input"
         />
-        <button>채팅 입력</button>
+        <button className="chat-button">
+          <AiOutlineEnter />
+        </button>
       </form>
     </ChatInputContainer>
   );
