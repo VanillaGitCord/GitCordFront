d63f2145b7b231569098347f21aa55675df7b5f2
takhyunKim <kimkih1218@gmail.com>
2021-04-20 21:55:46 +0900

    Update: userToolbar icon style

--- src/components/Main/UserList/UserToolbar/UserToolbar.jsx
+++ src/components/Main/UserList/UserToolbar/UserToolbar.jsx
@@ -9,11 +9,19 @@ import { GiSpeakerOff, GiSpeaker } from "react-icons/gi";
 import styled from "styled-components";
 
 const UserToolbarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   width: 100%;
   height: 10%;
+  background-color: #FFEEF4;
   bottom: 0;
   border-radius: 10px;
-  background-color: #FFEEF4;
+  font-weight: bold;
+
+  .toolbar-icon {
+    margin: 10px;
+  }
 `;
 
 function UserToolbar({ user }) {
@@ -21,11 +29,11 @@ function UserToolbar({ user }) {
     <UserToolbarContainer>
       {user.name}
       <article>
-        <GiSpeaker />
-        <BsFillMicFill />
-        <AiFillCaretLeft />
-        <AiFillCaretRight />
-        <AiFillSave />
+        <GiSpeaker size={25} className="toolbar-icon" />
+        <BsFillMicFill size={25} className="toolbar-icon" />
+        <AiFillCaretLeft size={25} className="toolbar-icon" />
+        <AiFillCaretRight size={25} className="toolbar-icon" />
+        <AiFillSave size={25} className="toolbar-icon" />
       </article>
     </UserToolbarContainer>
   );
